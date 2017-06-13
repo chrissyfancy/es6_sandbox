895f01119580966e2dd4eb443c50f693dd09f961
Chrissy Fancy <chrissyfancy@gmail.com>
2017-06-13 15:36:14 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// understand what "this" is
+
+// `this` is the Global space (e.g., "Window")
+
+// `this` is used within a function to reference where the function
+// is initially defined
+
+// this function is defined with ES5 classic function syntax
+// we can change what `this` refers to
+let sayHello = function() {
+  console.log("inside SayHello")
+  console.log(this)
+  console.log("Hello")
+}
+
+// this function is defined with the ES6 arrow syntax
+// we can't alter the `this` using `bind`
+let cantChangeThis = () => {
+  console.log("inside SayHello")
+  console.log(this)
+  console.log("Hello")
+}
+
+let outerFunction = () => {
+  sayHello();
+}
+
+let frog = {
+  name: "Kermit"
+}
+
+let doggie = {
+  doggieHello() {
+    console.log("Inside doggieHello, this refers to:")
+    console.log(this)
+    console.log("woof")
+  }
+}
